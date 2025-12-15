928b8a589f2c75c24c1d38410cb394f99293f646
root <preprint@pensoft.net>
2025-12-15 16:03:54 +0200

    menu sidebar style

--- assets/js/app.js
+++ assets/js/app.js
@@ -390,6 +390,16 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
         return false;
     });
+
+
+    initDesktopMenuToggle();
+
+    initHamburgerMenuDropdowns();
+
+    initNavbarScrollHandler();
+
+    initSearchToggles();
+
 });
 
 
@@ -445,75 +455,74 @@ function encodeURIObject(data){
     }).join('&');
 }
 
-function appendProfile() {
-    $(document).on('profile', function (e) {
-        var headerNavbarNav = $('#headerNavbarNav');
-        var li = '<li class="nav-item"><a href="/profile" target = "_self">Profile</a></li >';
-        headerNavbarNav.find('>ul').append(li);
-    });
-}
-function appendSignIn(){
-    $(document).on('signin', function (e) {
-        var headerNavbarLogin = $('#headerNavbarNav');
-        var li = '<li class="nav-item sign-in"><a href="/login" target = "_self">Login</a></li >';
-		headerNavbarLogin.find('>ul').append(li);
-		var menu = $('#menuToggle');
-		menu.find('>ul').append(li);
-    });
-}
-
-function appendSignOut() {
-    $(document).on('signout', function (e) {
-        var headerNavbarNav = $('#headerNavbarNav');
-        var li = '<li class="nav-item  sign-in"><a data-request="onLogout" data-request-data="redirect: \'/\'">Logout</a></li >';
-        headerNavbarNav.find('>ul').append(li);
-		var menu = $('#menuToggle');
-		menu.find('>ul').append(li);
-    });
-}
-
-function appendSearchAndSocialMedia(){
-	var liSearch = '<li class="nav-item search_field"><a href=\"javascript: void(0);\" onclick=\"showSearchForm();\"></a></li>';
-	var liSocial = '<li class="nav-item social">' +
-        '<a href=\"https://twitter.com/Know_Center\" target=\"_blank\" class=\"pr p-twitter normal\" target=\"_blank\"></a>' +
-        '<a href=\"https://www.linkedin.com/company/tier2-project/\" target=\"_blank\" class=\"pr p-linkedin normal\" target=\"_blank\"></a>' +
-        '<a href=\"https://www.youtube.com/@tier2project\" target=\"_blank\" class=\"pr p-youtube normal\" target=\"_blank\"></a></li>';
-	var menu = $('#menuToggle');
-	menu.find('>ul').append(liSearch).append(liSocial);
-}
-
-// function redirectAndRefresh(url){
-// 	$(".tabs a").each(function() {
-// 		this.href = window.location.hash;
-// 	});
-// 	window.open(url, '_blank');
-// 	location.reload();
-// }
-
 function isBreakpointLarge() {
     return window.innerWidth <= 991;
 }
 
-function showSearchForm(){
-	// if ($(".search").is(':visible')) {
-	// 	$('#menu').show();
-	// } else {
-	// 	$(".search").slideDown(300);
-	// 	$('#menu').hide();
-	// }
-	// $('#menu').hide();
-	$('#layout-header').toggleClass('full-width');
-	$('#search').toggle();
-	$('#menu li').hide();
-	$('nav a:not(.navbar-brand)').hide();
+
+// Function to show the search form
+function showSearchForm() {
+    // Simple fade in with a pop effect
+    $('#search').fadeIn(200);
+    $('#search form').addClass('pop-in');
+
+    // Clear any previous search text and focus the input
+    $('#search input.search_input').val('').focus();
+
+    $('body').addClass('search-open');
+
+    // Prevent scrolling when search is open
+    $('body').css('overflow', 'hidden');
+
+    // Add event listener to close search when clicking outside
+    $(document).on('click.searchClose', function(event) {
+        const $search = $('#search form');
+        const $searchToggle = $('#searchToggle');
+
+        // If click is outside search container and not on search button
+        if (!$search.is(event.target) &&
+            $search.has(event.target).length === 0 &&
+            !$searchToggle.is(event.target) &&
+            $searchToggle.has(event.target).length === 0 &&
+            !$(event.target).closest('.close-search').length) {
+            hideSearchForm();
+        }
+    });
+
+    // Add escape key handler
+    $(document).on('keydown.searchEscape', function(e) {
+        if (e.key === 'Escape') {
+            hideSearchForm();
+        }
+    });
+
+    // Add enter key handler to submit the form
+    $('#search input.search_input').on('keydown.searchSubmit', function(e) {
+        if (e.key === 'Enter') {
+            $('#search form').submit();
+        }
+    });
 }
 
-function hideSearchForm(){
-	$('#layout-header').toggleClass('full-width');
-	$('#search').hide();
-	$('#menu li').show();
-    $('nav a').show();
+// Function to hide the search form
+function hideSearchForm() {
+    // Simple fade out
+    $('#search form').removeClass('pop-in');
+    $('#search').fadeOut(200);
+
+    $('body').removeClass('search-open');
+
+    // Restore scrolling
+    $('body').css('overflow', '');
+
+    // Remove the document event listeners
+    $(document).off('click.searchClose');
+    $(document).off('keydown.searchEscape');
+    $('#search input.search_input').off('keydown.searchSubmit');
 }
+
+
+
 
 function requestFormLibrary() {
 	$('#mylibraryForm').on('click', 'a', function () {
@@ -937,7 +946,7 @@ function init() {
             }
         }
 		// appendSearchAndSocialMedia()
-		requestFormLibrary()
+		requestFormLibrary();
 
 		// requestFormPartners()
         // keepFooter(documentHasScroll());
@@ -948,6 +957,311 @@ function init() {
     // appendSignIn()
     // appendSignOut()
 }
+
+
+/* **** */
+/* **** */
+/* **** */
+/* **** */
+
+function initSearchToggles() {
+    $('#searchToggle, #searchToggleMobile').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        showSearchForm();
+    });
+}
+
+function initNavbarScrollHandler() {
+    const $headerNavbar = $('#headernavbar');
+    const $backToTop = $('.back-to-top');
+    let hasShownBackToTop = false;
+
+    $(window).on('scroll.navbar', function() {
+        const currentScrollTop = $(this).scrollTop();
+        const windowHeight = $(window).height();
+        const documentHeight = $(document).height();
+        const scrollPercent = currentScrollTop / (documentHeight - windowHeight);
+
+        if (currentScrollTop > 80) {
+            $headerNavbar.addClass('navbar-scrolled');
+        } else {
+            $headerNavbar.removeClass('navbar-scrolled');
+        }
+
+        if (currentScrollTop > 300) {
+            if (!$backToTop.hasClass('show')) {
+                $backToTop.addClass('show');
+                if (!hasShownBackToTop) {
+                    $backToTop.addClass('first-show');
+                    hasShownBackToTop = true;
+                    setTimeout(function() {
+                        $backToTop.removeClass('first-show');
+                    }, 600);
+                }
+            }
+        } else {
+            $backToTop.removeClass('show');
+        }
+
+        updateScrollProgress(scrollPercent);
+    });
+}
+
+
+/**
+ * Initialize hamburger menu dropdown functionality
+ * Handles dropdown menu toggles, auto-expand, and menu state management
+ */
+function initHamburgerMenuDropdowns() {
+    // Auto-expand dropdowns that contain the current active page
+    function autoExpandActiveDropdowns() {
+        const activeSubItems = $('#headerNavbarNav .dropdown-menu .nav-item.active');
+
+        activeSubItems.each(function() {
+            // Find the parent dropdown
+            const parentDropdown = $(this).closest('.nav-item.dropdown');
+            if (parentDropdown.length) {
+                const dropdownMenu = parentDropdown.find('.dropdown-menu');
+
+                // Expand the parent dropdown
+                parentDropdown.addClass('active');
+                if (dropdownMenu.length) {
+                    dropdownMenu.addClass('show');
+                }
+            }
+        });
+    }
+
+    // Run auto-expand on page load
+    autoExpandActiveDropdowns();
+
+    // Handle dropdown menu toggles
+    const dropdownItems = $('#headerNavbarNav .nav-item.dropdown > a');
+
+    dropdownItems.each(function() {
+        $(this).off('click.dropdown').on('click.dropdown', function(e) {
+            e.preventDefault();
+
+            const parentItem = $(this).parent();
+            const dropdownMenu = parentItem.find('.dropdown-menu');
+
+            if (dropdownMenu.length) {
+                // Toggle active state on parent item
+                parentItem.toggleClass('active');
+
+                // Toggle show state on dropdown menu
+                dropdownMenu.toggleClass('show');
+
+                // Optional: Close other open dropdowns (accordion behavior)
+                const otherDropdowns = $('#headerNavbarNav .nav-item.dropdown');
+                otherDropdowns.each(function() {
+                    if (this !== parentItem[0]) {
+                        $(this).removeClass('active');
+                        const otherMenu = $(this).find('.dropdown-menu');
+                        if (otherMenu.length) {
+                            otherMenu.removeClass('show');
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    // Close all dropdowns when menu is closed (but preserve auto-expanded state)
+    function closeAllDropdowns() {
+        const activeDropdowns = $('#headerNavbarNav .nav-item.dropdown.active');
+        activeDropdowns.each(function() {
+            $(this).removeClass('active');
+            const menu = $(this).find('.dropdown-menu');
+            if (menu.length) {
+                menu.removeClass('show');
+            }
+        });
+    }
+
+    function handleMenuToggle() {
+        // When menu is opened, auto-expand dropdowns with active items
+        setTimeout(function() {
+            autoExpandActiveDropdowns();
+        }, 100); // Small delay to ensure menu animation completes
+    }
+
+    const closeMenuBtn = $('#closeMenuBtn');
+    if (closeMenuBtn.length) {
+        closeMenuBtn.off('click.dropdown').on('click.dropdown', closeAllDropdowns);
+    }
+
+    // Re-expand dropdowns when menu is opened
+    const menuToggleBtn = $('#desktopMenuToggle');
+    if (menuToggleBtn.length) {
+        menuToggleBtn.off('click.dropdown').on('click.dropdown', handleMenuToggle);
+    }
+
+    // Close dropdowns when clicking outside
+    $(document).off('click.dropdownOutside').on('click.dropdownOutside', function(e) {
+        const navbar = $('#headerNavbarNav');
+        const menuToggle = $('#desktopMenuToggle');
+
+        if (navbar.length && !navbar.is(e.target) && navbar.has(e.target).length === 0 &&
+            !menuToggle.is(e.target) && menuToggle.has(e.target).length === 0) {
+            closeAllDropdowns();
+        }
+    });
+}
+
+
+function initDesktopMenuToggle() {
+    const $navbarNav = $('#headerNavbarNav');
+    const $desktopToggle = $('#desktopMenuToggle');
+    const $closeBtn = $('#closeMenuBtn');
+
+    $desktopToggle.on('click.desktopMenu', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $navbarNav.addClass('show').css({
+            right: '0',
+            opacity: '1',
+            visibility: 'visible'
+        });
+        $desktopToggle.hide();
+        $('body').addClass('menu-open');
+    });
+
+    $closeBtn.on('click.desktopMenu', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $navbarNav.removeClass('show').css({
+            right: '-300px',
+            opacity: '0',
+            visibility: 'hidden'
+        });
+        $desktopToggle.show();
+        $('body').removeClass('menu-open');
+    });
+
+    $(document).on('click.desktopMenuOutside', function(event) {
+        if (
+            $navbarNav.hasClass('show') &&
+            !$navbarNav.is(event.target) &&
+            $navbarNav.has(event.target).length === 0 &&
+            !$desktopToggle.is(event.target) &&
+            $desktopToggle.has(event.target).length === 0 &&
+            !$closeBtn.is(event.target) &&
+            $closeBtn.has(event.target).length === 0
+        ) {
+            $navbarNav.removeClass('show').css({
+                right: '-300px',
+                opacity: '0',
+                visibility: 'hidden'
+            });
+            $desktopToggle.show();
+            $('body').removeClass('menu-open');
+        }
+    });
+
+    // Prevent clicks on the menu and bottom elements from closing it
+    $navbarNav.on('click.desktopMenu', function(e) { e.stopPropagation(); });
+    $('.navbar-bottom-elements').on('click.desktopMenu', function(e) { e.stopPropagation(); });
+}
+
+function initDesktopDropdownToggle() {
+    $('.nav-item.dropdown > a').on('click.desktopDropdown', function(e) {
+        e.preventDefault();
+        const $dropdownMenu = $(this).siblings('.dropdown-menu');
+        if ($dropdownMenu.hasClass('show')) {
+            $dropdownMenu.removeClass('show');
+        } else {
+            $('.dropdown-menu.show').removeClass('show');
+            $dropdownMenu.addClass('show');
+        }
+    });
+}
+
+function applyExternalLinkBehavior() {
+    $('.work_packages .accordion-content, .messages .accordion-toggle').each(function(index, value) {
+        $(value).find('a').attr('onclick', "window.open(this.href, '_blank');");
+    });
+}
+
+function sanitizeNavDropdowns() {
+    $('.nav-item').children('a').each(function() {
+        const $link = $(this);
+        // Skip footer navigation; it has its own dropdown handling
+        if ($link.closest('.footer-navigation').length) {
+            return;
+        }
+        if ($link.attr('data-toggle') === 'dropdown') {
+            $link
+                .removeAttr('data-toggle')
+                .off('click.navSanitize')
+                .on('click.navSanitize', function(e) {
+                    e.preventDefault();
+                    $(this).siblings('.dropdown-menu').toggleClass('show');
+                });
+        }
+    });
+}
+
+function initSmoothAnchorScroll() {
+    if (window.location.hash) {
+        const link = window.location.hash;
+        const anchorId = link.substr(link.indexOf('#') + 1);
+        if ($('#' + anchorId).offset()) {
+            $('html, body').animate({
+                scrollTop: $('#' + anchorId).offset().top - 150
+            }, 500);
+        } else {
+            $('.accordion-border').each(function() {
+                const title = $(this).find('.accordion-toggle .col-xs.start-xs').text().toUpperCase();
+                const toggler = $(this).find('.accordion-toggle');
+                if (title.indexOf(anchorId.toUpperCase()) >= 0 && !toggler.next('.accordion-content').is(':visible')) {
+                    $('html, body').animate({
+                        scrollTop: toggler.parent().offset().top - 150
+                    }, 500);
+                    toggler.trigger('click');
+                }
+            });
+        }
+    }
+
+    $('.dropdown a').on('click.anchor', function(event) {
+        if (location.href.indexOf('#') !== -1) {
+            const link = $(this).attr('href');
+            const anchorId = link.substr(link.indexOf('#') + 1);
+            if ($('#' + anchorId).length > 0) {
+                $('html, body').animate({
+                    scrollTop: $('#' + anchorId).offset().top - 150
+                }, 500);
+            } else {
+                $("g[title='" + anchorId.toUpperCase() + "']").addClass('active_path');
+
+                $('.accordion-border').each(function() {
+                    const title = $(this).find('.accordion-toggle .col-xs.start-xs').text().toUpperCase();
+                    const toggler = $(this).find('.accordion-toggle');
+                    if (title.indexOf(anchorId.toUpperCase()) >= 0 && !toggler.next('.accordion-content').is(':visible')) {
+                        $('html, body').animate({
+                            scrollTop: toggler.parent().offset().top - 150
+                        }, 500);
+                        toggler.trigger('click');
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+    });
+
+    onHashChange();
+    $(window).on('hashchange', function() {
+        onHashChange();
+    });
+}
+
+
+/* **** */
+/* **** */
+/* **** */
+/* **** */
 
 
 function handlePilotsSVGMapMouseMove(event) {
@@ -1010,5 +1324,6 @@ function onPilots(pTitle) {
     }
 
 }
+
 
 init()
