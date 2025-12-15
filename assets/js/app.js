c38a9318967eadd415415b11b6ca08abbec6b1f3
root <preprint@pensoft.net>
2025-12-15 17:28:39 +0200

    cookie

--- assets/js/app.js
+++ assets/js/app.js
@@ -400,9 +400,48 @@ $(document).ready(function() {
 
     initSearchToggles();
 
+
+
 });
 
 
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    // $('#cookieAcceptBar').hide();
+    let info = getCookie("finalEventInfoMessage");
+    if (info) {
+        // cookie is set
+        $('#event-information').hide();
+    }
+}
+
+function closeEventInfoBar(){
+    setCookie('finalEventInfoMessage', 1, 90);
+    $('#event-information').toggle("slow");
+}
 
 
 
@@ -411,8 +450,6 @@ function openEventModalBtn(){
         $(".openEventModalBtn").trigger("click");
     },10);
 }
-
-
 
 
 function onHashChange(){
@@ -947,6 +984,8 @@ function init() {
         }
 		// appendSearchAndSocialMedia()
 		requestFormLibrary();
+
+        checkCookie();
 
 		// requestFormPartners()
         // keepFooter(documentHasScroll());
